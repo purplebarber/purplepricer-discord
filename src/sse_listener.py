35cdb899824ed6554d98fc964188c5bf4bb77bf0
Purple <87976511+purplebarber@users.noreply.github.com>
2023-08-21 11:37:10 +0800

    added error handling

--- src/sse_listener.py
+++ src/sse_listener.py
@@ -5,6 +5,8 @@ from src.http import HTTP
 from src.config import Config
 from src.webhook import Webhook
 from src.database import Database
+from aiohttp import ClientConnectorError
+from time import sleep
 
 DB_NAME = "src/prices.db"
 
@@ -18,6 +20,7 @@ class SSEListener:
         self.headers = self.config.headers
         self.params = self.config.params
         self.database = Database(db_name=DB_NAME, table_name="prices")
+        self.sse_response = None
 
     async def update_pricelist(self) -> None:
         prices = await self.http.get(self.config.pricelist_url, headers=self.config.headers, params=self.config.params)
@@ -42,34 +45,45 @@ class SSEListener:
         return requests.get(self.sse_url, stream=True, headers=self.headers, params=self.params)
 
     async def listen_and_update(self) -> None:
-        response = self.sse_request()
-        client = sseclient.SSEClient(response)
-        for event in client.events():
-            if not event.event == "price":
-                print("Event not price")
-                continue
+        while True:
+            try:
+                if self.sse_response is None:
+                    self.sse_response = self.sse_request()
+                client = sseclient.SSEClient(self.sse_response)
+                for event in client.events():
+                    if not event.event == "price":
+                        print("Event not price")
+                        continue
 
-            data = loads(event.data)
-            sku = data.get("sku")
-            name = data.get("name")
-            buy = data.get("buy")
-            sell = data.get("sell")
+                    data = loads(event.data)
+                    sku = data.get("sku")
+                    name = data.get("name")
+                    buy = data.get("buy")
+                    sell = data.get("sell")
 
-            if not sku or not name or not buy or not sell:
-                print("Missing data")
-                continue
+                    if not sku or not name or not buy or not sell:
+                        print("Missing data")
+                        continue
 
-            old_item_data = self.database.get_item_data(sku)
-            if not old_item_data:
-                old_item_data = {"name": name, "buy": buy, "sell": sell,
-                                 "old_buy": buy, "old_sell": sell}
+                    old_item_data = self.database.get_item_data(sku)
+                    if not old_item_data:
+                        old_item_data = {"name": name, "buy": buy, "sell": sell,
+                                         "old_buy": buy, "old_sell": sell}
 
-            item_data = {"name": name, "buy": buy, "sell": sell,
-                         "old_buy": old_item_data.get("buy"),
-                         "old_sell": old_item_data.get("sell")}
+                    item_data = {"name": name, "buy": buy, "sell": sell,
+                                 "old_buy": old_item_data.get("buy"),
+                                 "old_sell": old_item_data.get("sell")}
 
-            self.database.insert_item_data(sku, item_data)
-            await self.webhook.send_webhooks_to_urls(sku, name, item_data)
+                    self.database.insert_item_data(sku, item_data)
+                    await self.webhook.send_webhooks_to_urls(sku, name, item_data)
+
+            except ClientConnectorError as connector_error:
+                print(f"Client Connector Error: {connector_error}")
+                sleep(5)
+
+            except Exception as e:
+                print(f"Exception: {e}")
+                sleep(5)
 
     async def close(self) -> None:
         await self.webhook.close()
